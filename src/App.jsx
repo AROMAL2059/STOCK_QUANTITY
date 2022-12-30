ee4f6d8238f510e93bd7f8249a47f620c19a1ade
Aromal <aromal2059@gmail.com>
2022-12-30 17:25:42 +0530

    save to github

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react'
 import "./App.css"
+import styled from 'styled-components';
 
 const App = () => {
   const [maxiloss, setMaxiloss] = useState('');
   const [totaltrade, setTotaltrade] = useState('');
   const [lossptrade, setLossptrade] = useState(0);
-  const [entry,setEntry]=useState('');
+  const [entry, setEntry] = useState('');
   const [stoploss, setStoploss] = useState('');
   const [totalquantity, setTotalquantity] = useState(0);
 
@@ -21,11 +22,11 @@ const App = () => {
   }
   function calculate() {
     const result = Math.floor(maxiloss / totaltrade);
-    const rupeestop=entry-stoploss;
-    const result2=Math.abs(Math.floor(result/rupeestop));
+    const rupeestop = entry - stoploss;
+    const result2 = Math.abs(Math.floor(result / rupeestop));
     setLossptrade(result);
     setTotalquantity(result2);
-    console.log(result,rupeestop,result2)
+    console.log(result, rupeestop, result2)
 
   }
 
@@ -37,27 +38,28 @@ const App = () => {
     setStoploss(e.target.value);
 
   }
- 
+
   useEffect(() => {
     calculate();
-  }, [totaltrade, maxiloss,entry,stoploss])
+  }, [totaltrade, maxiloss, entry, stoploss])
 
 
   return (
-    <div>
+    <Wrapper>
+
       <div className='asahole'>
         <div>
-        <label>Total Capital</label>
-        <input type='text' placeholder='0'></input>
-        <label>Total Number Of Trades</label>
-        <input type='text' placeholder='0' value={totaltrade} onChange={handletotaltradechange}></input>
-        <label>Maxium loss</label>
-        <input type='text' placeholder='0' value={maxiloss} onChange={handlemaxilosschange}></input>
-        <div className='losspertrade'>
-          <label>Loss/Trade</label>
-          <span>{lossptrade}</span>
+          <label>Total Capital</label>
+          <input type='text' placeholder='0'></input>
+          <label>Total Number Of Trades</label>
+          <input type='text' placeholder='0' value={totaltrade} onChange={handletotaltradechange}></input>
+          <label>Maxium loss</label>
+          <input type='text' placeholder='0' value={maxiloss} onChange={handlemaxilosschange}></input>
+          <div className='losspertrade'>
+            <label>Loss/Trade</label>
+            <span>{lossptrade}</span>
 
-        </div>
+          </div>
         </div>
 
         <div>
@@ -71,11 +73,46 @@ const App = () => {
 
           </div>
         </div>
-        </div>
+      </div>
 
 
-    </div>
+
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+
+label{
+  display: block;
+  text-transform: uppercase;
+  font-size: .8rem;
+  line-height: .5rem;
+  padding-left: 1px;
+  margin-bottom: .55rem;
+}
+input[type="text"]{
+  background:transparent;
+  border: none;
+  color: azure;
+  margin-bottom: 10px;
+  font-size: 1.4rem;
+}
+.losspertrade{
+  background: #158;
+  padding: 10px;
+  border-radius: 10px;
+  font-size: 2rem;
+  
+}
+.asahole{
+  background: rebeccapurple;
+  padding: 10px;
+  border-radius: 10px;
+  display: flex;
+ 
+  justify-content: space-between;
+}
+`
 
 export default App
